60348fa3d84ef51b8faca15e19fa78bd39a423bc
CITATS928 <93238522+CITATS928@users.noreply.github.com>
2024-07-10 09:30:14 -0400

    new version

--- multilingual_greeter.py
+++ multilingual_greeter.py
@@ -1,4 +1,45 @@
 #Exercise2
+def language_input() -> int:
+    print("Please choose a language: ")
+    print("1 - English")
+    print("2 - Spanish")
+    print("3 - Portuguese")
+    return int(input())
+
+
+def name_input(lang_choice: int) -> str:
+
+    prompt = None
+
+    if lang_choice == 2:
+        prompt = "¿Cómo te llamas?\n"
+    elif lang_choice == 3:
+        prompt = "Qual é o seu nome?\n"
+    else:
+        prompt = "What is your name?\n"
+
+    return input(prompt)
+
+
+def greet(name: str, lang_choice: int) -> None:
+
+    greeting = None
+
+    if lang_choice == 2:
+        greeting = "Hola " + name
+    elif lang_choice == 3:
+        greeting = "Olá " + name
+    else:
+        greeting = "Hello " + name
+    
+    print(greeting)
+
+
+lang_choice = language_input()
+name = name_input(lang_choice)
+greet(name, lang_choice)
+
+"""
 def greet(name):
     print("Hi "+name)
 
@@ -18,3 +59,4 @@ def language_input(name):
         print("invalid input")
 
 language_input(name_input())
+"""
